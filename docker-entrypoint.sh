51c23b24e0eb4e8c15c98d0f87a8c4115618848a
Arafat Olayiwola <harof.dev@gmail.com>
2025-05-16 08:58:24 +0100

    chore: remove terraform

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,25 +1,15 @@
-# #!/bin/bash
-
-# # Start Django server
-# python manage.py runserver 0.0.0.0:8000 &
-
-# # Start Celery Worker
-# celery -A EngageX_Streaming worker --loglevel=info --pool=threads &
-
-# # Start Celery Beat
-# celery -A EngageX_Streaming flower --loglevel=info &
-
-# # Wait for any of the background processes to exit
-# wait -n
-
-
 #!/bin/bash
+
+# Start Django server
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn EngageX_Streaming.wsgi:application --bind 0.0.0.0:8000
+gunicorn EngageX_Streaming.wsgi:application --bind 0.0.0.0:8000 &
 
-# Start Celery worker
-celery -A EngageX_Streaming worker --loglevel=info --pool=threads
+# Start Celery Worker
+celery -A EngageX_Streaming worker --loglevel=info --pool=threads &
 
-# Start Flower for monitoring Celery tasks
-celery -A EngageX_Streaming flower --loglevel=info
+# Start Celery Beat
+celery -A EngageX_Streaming flower --loglevel=info &
+
+# Wait for any of the background processes to exit
+wait -n
