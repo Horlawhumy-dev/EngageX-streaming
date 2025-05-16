820d559a554d06bdba94571dad8ada0f62f56509
Arafat Olayiwola <harof.dev@gmail.com>
2025-05-16 19:40:32 +0100

    fix redis ssl

--- EngageX_Streaming/celery.py
+++ EngageX_Streaming/celery.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
+import ssl
 from celery import Celery
 import multiprocessing
 multiprocessing.set_start_method("spawn", force=True)
@@ -8,8 +9,8 @@ multiprocessing.set_start_method("spawn", force=True)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EngageX_Streaming.settings")
 
 app = Celery("EngageX_Streaming",
-    # broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
-    # redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+    broker_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
+    redis_backend_use_ssl={"ssl_cert_reqs": ssl.CERT_NONE},
     )
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
